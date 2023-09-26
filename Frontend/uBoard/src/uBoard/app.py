a5acb28227fa5e9440aed9845e66a0e8606fb7b8
SuneKR <SuneKRmail@gmail.com>
2023-09-26 13:49:17 +0200

    connection test to api and a bit of cleanup

--- Frontend/uBoard/src/uBoard/app.py
+++ Frontend/uBoard/src/uBoard/app.py
@@ -4,7 +4,16 @@ Crossplatform frontend of the uboard
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
+import httpx
 
+def pieceAdder(name):
+    if name: return f"{name} have been added"
+    else: return "Can't be added"
+
+def pieceFinder(selv, widget):
+    with httpx.Client() as client:
+        reponse = client.get("http://127.0.0.1:8000/piece/")
+    payload = reponse.json()
 
 class uBroardSurface(toga.App):
 
@@ -27,16 +36,26 @@ class uBroardSurface(toga.App):
         name_box.add(self.name_input)
         
         button = toga.Button("Give me your piece of mind!", on_press=self.pieceOfMind, style=Pack(padding=5)) #it's funny because its a pun!
+        buttonShow = toga.Button("show me your piece", on_press=self.pieceOfMind, style=Pack(padding=5)) #this is also funny because its a pun!
         
         main_box.add(name_box)
         main_box.add(button)
+        main_box.add(buttonShow)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
         
-    def pieceOfMind(self, widget): #it's still funny because its a pun!
-        print(f"{self.name_input.value} have added to a board")
+    async def pieceOfMind(self, widget): #it's still funny because its a pun!
+        with httpx.AsyncClient() as client:
+            #response = client.get("http://127.0.0.1:8000/piece/")
+            response = await client.get("http://127.0.0.1:8000/piece/6510af927f02845b2fdf708a")
+            #response = client.get("https://jsonplaceholder.typicode.com/posts/42")
+        
+        payload = response.json()
+        
+        self.main_window.info_dialog(pieceAdder(self.name_input.value), "{name}: ({x},{y}), Size={size}".format(name=payload["name"], x=payload["xPos"], y=payload["yPos"], size=payload["size"]),)
+    
 
 
 
